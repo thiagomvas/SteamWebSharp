c93dacbb1aa59298185bcc14fa0c16631a63fd0b
Thiago Menezes <thiagomvas@gmail.com>
2025-01-06 11:17:20 -0300

    fix: Fix namespaces

--- SteamWebSharp/SteamUserStatsEndpoints.cs
+++ SteamWebSharp/SteamUserStatsEndpoints.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SteamWebSharp.Models;
 
 namespace SteamWebSharp;
 internal class SteamUserStatsEndpoints : ISteamUserStats
